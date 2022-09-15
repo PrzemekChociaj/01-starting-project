cf8f82a49916432aa9b03694572df17186457b69
PrzemekChociaj <111577035+PrzemekChociaj@users.noreply.github.com>
2022-09-15 20:42:58 +0200

    Projekt-intross
    projekt

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUser from './components/AddUser';
+import ShowUp from './components/Card';
+
+function App() {
+	return <div>
+		<AddUser />
+		<ShowUp />
+	</div>;
+}
+
+export default App;
